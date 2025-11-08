04b9761106cb2199764bebd52b13a2490da99383
Jehoshua M <jehoshua.dev@gmail.com>
2025-11-08 13:57:53 +0530

    Connect Inngest

---
+++ lib/inngest.ts
@@ -0,0 +1,33 @@
+import { Inngest } from "inngest";
+import {connectDB} from "@/lib/db";
+import User from "@/app/models/User";
+
+export const inngest = new Inngest({ id: "scythe-client"});
+
+const syncUser = inngest.createFunction(
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
+        await connectDB();
+        const { id, email_addresses, first_name, last_name, image_url } = event.data;
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0]?.email_addresses,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage: image_url,
+        }
+        await User.create(newUser);
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    { id: "delete-user-from-db" },
+    { event: "clerk/user.deleted" },
+    async ({ event }) => {
+        await connectDB();
+        const { id } = event.data;
+        await User.deleteOne({ clerkId: id });
+    }
+)
+
+export const functions = [syncUser, deleteUserFromDB];
